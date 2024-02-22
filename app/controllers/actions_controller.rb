3613fa47dee4b79b2861ea7675afeacae47a7a26
kinoshita <al21059@shibaura-it.ac.jp>
2024-02-22 19:12:56 +0900

    今日含む週の記録を積み上げ棒グラフで表示する機能を実装
    今後、日にちを選べるようにし、日にち選択後はその日にちを含む週の記録を表示するようにアップグレードする

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,31 +1,51 @@
 class ActionsController < ApplicationController
-    def index
-        @start_of_week = Date.current.beginning_of_week
-        @end_of_week = Date.current.end_of_week
+  def index
+      @start_of_week = Date.current.beginning_of_week
+      @end_of_week = Date.current.end_of_week
 
-        @actions = Action.where(day: @start_of_week..@end_of_week)
-                        .group(:day, :category)
-                        .select("day, category, SUM(total_time) as total_time")
-    end
+      @actions = Action.where(day: @start_of_week..@end_of_week)
+                      .group(:day, :category)
+                      .select("day, category, SUM(total_time) as total_time")
+      
+      @categories = @actions.pluck(:category).uniq
+      @dates = (@start_of_week..@end_of_week).map { |date| date.strftime('%Y-%m-%d')}
 
-    def new
-      @action = Action.new
-    end
-
-    def create
-      @action = Action.new(action_params)
-      if @action.save
-        render json: { message: '行動記録が保存されました' }, status: :created
-      else
-        render json: { error: '行動記録の保存に失敗しました' }, status: :unprocessable_entity
+      @datasets = @categories.map do |category|
+        {
+          label: category,
+          data: @dates.map do |date|
+            total_time_on_date = @actions
+                                  .select { |action| action.category == category && action.day.to_s == date }
+                                  .reduce(0) { |sum, action| sum + action.total_time }
+          end,
+          backgroundColor: "#" + ("%06x" % (rand * 0xffffff))
+        }
       end
+      # json化
+      @actions_json = @actions.to_json.html_safe
+      @categories_json = @categories.to_json.html_safe
+      @dates_json = @dates.to_json.html_safe
+      @datasets_json = @datasets.to_json.html_safe
+  end
+
+  def new
+    @action = Action.new
+  end
+
+  def create
+    @action = Action.new(action_params)
+    if @action.save
+      render json: { message: '行動記録が保存されました' }, status: :created
+    else
+      render json: { error: '行動記録の保存に失敗しました' }, status: :unprocessable_entity
     end
+  end
 
 
-    private
+  private
 
-    def action_params
-      params.require(:action).permit(:start_time, :end_time, :total_time)
-    end
+  def action_params
+    params.require(:action).permit(:start_time, :end_time, :total_time)
+  end
 
 end
